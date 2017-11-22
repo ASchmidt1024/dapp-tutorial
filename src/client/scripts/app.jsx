3e41541429a85db05ba5ad5c3195daee1e731afc
Alexander Schmidt <alexander.schmidt@edvas.de>
2017-11-22 16:09:42 +0100

    start part 5

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import {Bond, TimeBond} from 'oo7';
-import {Rspan} from 'oo7-react';
-import {InputBond} from 'parity-reactive-ui';
-import {bonds, formatBlockNumber, formatBalance} from 'oo7-parity';
+import {Rspan, Rimg} from 'oo7-react';
+import {InputBond, HashBond} from 'parity-reactive-ui';
+import {bonds, formatBlockNumber, formatBalance, GitHubHintABI} from 'oo7-parity';
 
 export class App extends React.Component {
-
+	constructor() {
+		super();
+		this.bond = new Bond;
+		this.GithubHint = bonds.makeContract(bonds.registry.lookupAddress('githubhint', 'A'), GitHubHintABI);
+	}
 	render() {
 		return (
 			<div>
-				Default account:&nbsp;
-				<Rspan>{bonds.me}</Rspan>&nbsp;
-				<br/>Given the name of&nbsp;<Rspan>
-					{bonds.accountsInfo[bonds.me].name}
-				</Rspan>
-				<br/>With a balance of&nbsp;
-				<Rspan>
-					{bonds.balance(bonds.me).map(formatBalance)}
-				</Rspan>
+				<InputBond bond={this.bond} placeholder='Name' />
+				<Rimg src={this.GithubHint.entries(bonds.registry.lookupData(this.bond, 'IMG'))[0]} /> 
+				<Rspan>{this.GithubHint.entries(bonds.registry.lookupData(this.bond, 'IMG'))[0]}</Rspan>
 			</div>
 		);
 	}
